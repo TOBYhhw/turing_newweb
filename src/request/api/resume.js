67fba0ea09bd0bffb06a011e97a6c710b9935b4b
TOBYhhw <1505994122@qq.com>
2022-11-04 17:05:19 +0800

    封装简历接口

---
+++ src/request/api/resume.js
@@ -0,0 +1,35 @@
+import myAxios from "../axios";
+
+//面试简历模板下载地址
+export function getMobanUrlAPI() {
+    return myAxios({
+        url: "/guest/resume/getMobanUrl",
+        method: 'get',
+    })
+}
+
+//创业组内容填写
+export function resumeEntrepreneurshipAddAPI(data) {
+    return myAxios({
+        url: "/guest/resume/resumeEntrepreneurshipAdd",
+        method: 'post',
+        parmas: data
+    })
+}
+
+//创新组内容填写
+export function resumeInnovateAddAPI(data) {
+    return myAxios({
+        url: "/guest/resume/resumeInnovateAdd",
+        method: 'post',
+        parmas: data
+    })
+}
+//根据学号查询简历内容
+export function ResumeQueryByIdAPI(StudentId) {
+    return myAxios({
+        url: "/guest/resume/ResumeQueryById",
+        method: 'get',
+        params: { StudentId }
+    })
+}
